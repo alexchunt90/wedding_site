00bb661d5d96c238baa4d4cddbb1c1375937e188
Alex Hunt <alex.c.hunt90@gmail.com>
2018-04-08 16:38:08 -0400

    Figured out the parallax issue without monkey patching. Most of the site functionality is set up, now just to populate with content.

--- src/components/imageDivider.js
+++ src/components/imageDivider.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Section } from '../components/section'
 
 const ImageDivider = ({image}) =>
-	<div className='parallax-window d-flex h-100 flex-grow' data-parallax='scroll' data-image-src={'/static/images/' + image + '.jpg'}>
+	<div className='parallax-window d-flex h-50 flex-grow' data-parallax='scroll' data-image-src={'/static/images/' + image + '.jpg'}>
 	</div>
 
 export default ImageDivider
